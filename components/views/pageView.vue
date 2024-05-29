0bdd3ed72fc57eabe80536a4ff3df9f3f512012e
tutaru99 <simas.raudonis@gmail.com>
2024-05-29 12:32:12 +0200

    OS2UOL-387: Seo, robots added for simple pages.

--- components/views/pageView.vue
+++ components/views/pageView.vue
@@ -16,14 +16,16 @@ const robots = ref(
   props.data?.field_meta_tags?.html_head?.robots?.attributes?.content,
 );
 
+const seoPageContent = ref(
+  props.data?.field_meta_tags?.html_head?.description?.attributes?.content,
+);
+
 useHead({
   title: props?.data?.field_meta_tags?.html_head?.title?.attributes?.content,
   meta: [
     {
       name: 'description',
-      content:
-        props?.data?.field_meta_tags?.html_head?.description?.attributes
-          ?.content,
+      content: seoPageContent.value,
     },
     {
       name: 'robots',
