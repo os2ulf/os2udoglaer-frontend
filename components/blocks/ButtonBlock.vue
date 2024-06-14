53dcc4798f751e45167d3edd7f8ba6fb34f5e6c4
Søren Due Bonde <sdb@novicell.dk>
2024-06-14 21:21:16 +0200

    OS2UOL-108: Button style changes and alignment with design and section themes

--- components/blocks/ButtonBlock.vue
+++ components/blocks/ButtonBlock.vue
@@ -30,52 +30,7 @@ const props = defineProps({
 
 <style lang="postcss" scoped>
 .button {
-  position: relative;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
   min-width: 150px;
   min-height: 56px;
-  padding: 16px 32px;
-  overflow: hidden;
-  font-weight: 700;
-  font-family: var(--heading-font-family);
-  border-radius: 100px;
-  cursor: pointer;
-  word-break: break-word;
-}
-
-.button--ghost {
-  border: 1px solid var(--color-primary-lighten-3);
-  color: var(--color-primary);
-  background-color: transparent;
-  transition: all 0.3s ease-in-out;
-
-  &:hover {
-    color: var(--color-primary);
-    background-color: var(--color-white);
-    border: 1px solid var(--color-white);
-  }
-}
-
-.button--normal {
-  background-color: var(--color-primary);
-  color: var(--color-white);
-  border: 1px solid transparent;
-  transition: all 0.3s ease-in-out;
-
-  &:hover {
-    background-color: var(--color-primary-darken-2);
-  }
-}
-
-.button--center {
-  left: 50%;
-  transform: translateX(-50%);
-}
-
-.button--right {
-  left: 100%;
-  transform: translateX(-100%);
 }
 </style>
