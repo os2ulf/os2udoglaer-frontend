b420bf652bb4ee33a1693ff8e680ef99971b1bc3
tutaru99 <simas.raudonis@gmail.com>
2024-06-17 11:04:50 +0200

    OS2UOL-442: Conditional page title if no page header for simple pages

--- components/views/pageView.vue
+++ components/views/pageView.vue
@@ -39,9 +39,24 @@ useHead({
   <div>
     <PageHeader v-if="pageHeader" :page-block-header-data="pageHeader" />
 
+    <div class="container" v-else>
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+          <h1 class="page-title">{{ data?.label }}</h1>
+        </div>
+      </div>
+    </div>
+
     <Sections
       v-if="data && data.layout_builder__layout"
       :sections="data.layout_builder__layout"
     />
   </div>
 </template>
+
+<style lang="postcss" scoped>
+.page-title {
+  padding-top: 48px @(--md) 96px;
+  margin-bottom: 32px @(--md) 64px;
+}
+</style>
