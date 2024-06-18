7220dd9cecd4e77c4b1817006fe2f1d19c9344c3
tutaru99 <simas.raudonis@gmail.com>
2024-06-18 09:48:52 +0200

    OS2UOL-125: Moved Sted up below provider

--- components/views/internshipView.vue
+++ components/views/internshipView.vue
@@ -74,6 +74,35 @@ const practicalInfoData = computed(() => {
         },
       ],
     },
+
+    {
+      group: [
+        {
+          title: 'Sted',
+          content: [
+            props.data?.field_view_on_map == 'show_vendor_address' &&
+            props.data?.provider
+              ? props.data?.provider.field_location_name
+              : props.data?.field_location_name,
+            props.data?.field_view_on_map == 'show_vendor_address' &&
+            props.data?.provider
+              ? props.data?.provider.field_location_street
+              : props.data?.field_location_street,
+            props.data?.field_view_on_map == 'show_vendor_address' &&
+            props.data?.provider
+              ? props.data?.provider.field_location_zipcode
+              : props.data?.field_location_zipcode,
+            props.data?.field_view_on_map == 'show_vendor_address' &&
+            props.data?.provider
+              ? props.data?.provider.field_location_city
+              : props.data?.field_location_city,
+          ],
+          description: props.data?.field_location_description
+            ? props.data?.field_location_description
+            : '',
+        },
+      ],
+    },
     {
       group: [
         {
@@ -124,32 +153,6 @@ const practicalInfoData = computed(() => {
         {
           title: 'Huskeliste',
           content: props.data?.field_meeting_times,
-        },
-      ],
-    },
-    {
-      group: [
-        {
-          title: 'Sted',
-          content: [
-            props.data?.field_view_on_map == 'show_vendor_address' &&
-            props.data?.provider
-              ? props.data?.provider.field_location_name
-              : props.data?.field_location_name,
-            props.data?.field_view_on_map == 'show_vendor_address' &&
-            props.data?.provider
-              ? props.data?.provider.field_location_street
-              : props.data?.field_location_street,
-            props.data?.field_view_on_map == 'show_vendor_address' &&
-            props.data?.provider
-              ? props.data?.provider.field_location_zipcode
-              : props.data?.field_location_zipcode,
-            props.data?.field_view_on_map == 'show_vendor_address' &&
-            props.data?.provider
-              ? props.data?.provider.field_location_city
-              : props.data?.field_location_city,
-          ],
-          description: props.data?.field_location_description,
         },
       ],
     },
