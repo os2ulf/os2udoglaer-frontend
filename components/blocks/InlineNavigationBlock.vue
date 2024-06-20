49c384d43a021767bc069c09e31ee3ea1a3caf78
tutaru99 <simas.raudonis@gmail.com>
2024-06-20 16:18:08 +0200

    OS2UOL-114: Compact style inline nav + redid some data mapping since it changed on BE

--- components/blocks/InlineNavigationBlock.vue
+++ components/blocks/InlineNavigationBlock.vue
@@ -3,44 +3,45 @@ const props = defineProps({
   blockData: Object,
 });
 
-const inlineNavigationItems = ref(
-  props.blockData.field_inline_nav_paragraphs[0].field_inline_navigation_pages,
-);
+const inlineNavigationItems = ref(props.blockData.field_inline_nav_paragraphs);
 
-const altStyle = ref(true);
+const isAltStyle = ref(false);
 
-console.log('Inline Nav Items', inlineNavigationItems);
+console.log('Inline Nav Items', props.blockData);
 </script>
 
 <template>
   <div class="inline-navigation">
-    <div class="inline-navigation__card-wrapper" v-if="!altStyle">
+    <div class="inline-navigation__card-wrapper" v-if="!isAltStyle">
       <NuxtLink
-        :class="`inline-navigation__card-link`"
+        class="inline-navigation__card-link"
         v-for="item in inlineNavigationItems"
         :key="item"
-        :to="item.url"
-        :aria-label="`Link til ${item.label}`"
+        :to="item?.field_inline_navigation_pages?.url"
+        :aria-label="`Link til ${item?.field_inline_navigation_pages?.label}`"
       >
         <div class="inline-navigation__card-item">
           <div
             class="inline-navigation__card-image"
-            v-if="item.field_list_media"
+            v-if="item.field_inline_navigation_pages.field_list_media"
           >
             <BaseImage
-              v-if="item.field_list_media"
-              :image="item.field_list_media"
+              v-if="item.field_inline_navigation_pages.field_list_media"
+              :image="item.field_inline_navigation_pages.field_list_media"
             />
           </div>
           <div class="inline-navigation__card-content">
-            <h4 class="inline-navigation__card-title" v-if="item.label">
-              {{ item?.label }}
+            <h4
+              class="inline-navigation__card-title"
+              v-if="item.field_inline_navigation_pages.label"
+            >
+              {{ item?.field_inline_navigation_pages?.label }}
             </h4>
             <p
               class="inline-navigation__card-description"
-              v-if="item.field_description"
+              v-if="item.field_inline_navigation_pages.field_description"
             >
-              {{ item?.field_description }}
+              {{ item?.field_inline_navigation_pages?.field_description }}
             </p>
           </div>
           <div class="inline-navigation__card-link">
@@ -57,11 +58,11 @@ console.log('Inline Nav Items', inlineNavigationItems);
     <!-- Compact Style -->
     <div v-else class="inline-navigation__card-compact-wrapper">
       <NuxtLink
-        :class="`inline-navigation__card-link`"
+        class="inline-navigation__card-compact-link inline-navigation__card-link"
         v-for="item in inlineNavigationItems"
         :key="item"
-        :to="item.url"
-        :aria-label="`Link til ${item.label}`"
+        :to="item?.field_inline_navigation_pages?.url"
+        :aria-label="`Link til ${item?.field_inline_navigation_pages?.label}`"
       >
         <div class="inline-navigation__card-compact-item">
           <div class="inline-navigation__card-compact-container">
@@ -75,14 +76,16 @@ console.log('Inline Nav Items', inlineNavigationItems);
             <div class="inline-navigation__card-compact-content">
               <h4
                 class="inline-navigation__card-compact-title"
-                v-if="item.label"
+                v-if="item?.field_inline_navigation_pages?.label"
               >
-                {{ item?.label }}
+                {{ item?.field_inline_navigation_pages?.label }}
               </h4>
             </div>
           </div>
 
-          <div class="inline-navigation__card-link">
+          <div
+            class="inline-navigation__card-link inline-navigation__card-compact-button"
+          >
             <NuxtIcon
               class="inline-navigation__card-button-icon"
               name="arrow-right"
@@ -168,11 +171,16 @@ console.log('Inline Nav Items', inlineNavigationItems);
     gap: 22px;
   }
 
+  &__card-compact-button {
+    padding-left: 10px;
+  }
+
   &__card-compact-item {
+    color: var(--theme-color);
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 22px;
+    padding: 22px @(--sm) 32px;
     border: 2px solid var(--color-primary-lighten-4);
     border-radius: 4px;
     box-shadow: 0px 4px 10px 7px rgba(var(--color-primary-rgb), 0.1);
@@ -185,15 +193,12 @@ console.log('Inline Nav Items', inlineNavigationItems);
 
   &__card-compact-container {
     display: flex;
-  }
-
-  &__card-compact-content {
-    display: flex;
     align-items: center;
   }
 
   &__card-compact-title {
     margin-bottom: 0;
+    line-height: 28px;
   }
 
   &__card-compact-icon-container {
@@ -202,6 +207,8 @@ console.log('Inline Nav Items', inlineNavigationItems);
   }
 
   &__card-compact-icon {
+    display: flex;
+
     :deep(path:first-child) {
       stroke: var(--theme-color);
     }
