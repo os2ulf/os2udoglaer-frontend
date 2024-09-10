39a2dc8e3cc240630b603fa19ba43480b79f6809
Søren Due Bonde <sdb@novicell.dk>
2024-09-10 15:38:06 +0200

    OS2UOL-138: Changed provider/corporation email name to match backend

--- components/view-components/ContactForm.vue
+++ components/view-components/ContactForm.vue
@@ -59,7 +59,7 @@ const handleSubmit = async () => {
     message: trimmedMessage,
     phone: trimmedPhone,
     email: trimmedEmail,
-    provider_email: props.contactPersonEmail,
+    email_send_to: props.contactPersonEmail,
     url: props.currentUrl,
     title: props.currentTitle,
   };
