510f43f73eb26ccab1189ba81946b491063270f7
tutaru99 <simas.raudonis@gmail.com>
2025-01-29 13:56:45 +0100

    More debug added

--- utils/base64.ts
+++ utils/base64.ts
@@ -2,14 +2,19 @@ export const encodeBase64 = (value: string): string => {
   if (process.client) {
     return window.btoa(value);
   } else {
-    return Buffer.from(value, "ascii").toString("base64");
+    return Buffer.from(value, 'ascii').toString('base64');
   }
 };
 
 export const decodeBase64 = (value: string): string => {
-  if (process.client) {
-    return window.atob(value);
-  } else {
-    return Buffer.from(value, "base64").toString("ascii");
+  try {
+    if (process.client) {
+      return window.atob(value);
+    } else {
+      return Buffer.from(value, 'base64').toString('ascii');
+    }
+  } catch (error) {
+    console.error('Error in decodeBase64:', error);
+    throw error;
   }
 };
