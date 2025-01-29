ab795ca63c351abbbc477eda25f1852b4ad94e2d
tutaru99 <simas.raudonis@gmail.com>
2025-01-29 13:33:06 +0100

    New util method to handle negotiation for be domain for server only

--- server/api/submit-application-form.ts
+++ server/api/submit-application-form.ts
@@ -1,8 +1,7 @@
-import { useGetBackendDomain } from '~/composables/useGetBackendDomain';
+import { getBackendDomain } from '~/utils/getBackendDomainServer';
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
-  const beEndpoint = useGetBackendDomain();
 
   const body = await readBody(event);
   const credentials = `${config.REST_API_USER}:${config.REST_API_USER_PASS}`;
@@ -19,6 +18,11 @@ export default defineEventHandler(async (event) => {
   const currentDomain = normalizeDomain(event.node.req.headers.host);
   const requestOrigin = normalizeDomain(event.node.req.headers.origin);
 
+  console.log('current domain', currentDomain, requestOrigin);
+
+  const beEndpoint = getBackendDomain(currentDomain);
+  console.log('beEndpoint', beEndpoint);
+
   // BASIC ORIGIN HEADER CHECK
   // This is only a basic measure protection, real protection (if needed) should be implemented in the BE.
   if (!requestOrigin || requestOrigin !== currentDomain) {
@@ -29,7 +33,7 @@ export default defineEventHandler(async (event) => {
   }
 
   try {
-    console.log('beEndpoint', beEndpoint + '/node?_format=json');
+    console.log('beEndpoint w. node conc', beEndpoint + '/node?_format=json');
 
     const response = await fetch(beEndpoint + '/node?_format=json', {
       method: 'POST',
