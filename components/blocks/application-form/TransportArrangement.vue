4c2839f41277fad100447467659b54de5fe523ca
Søren Due Bonde <sdb@novicell.dk>
2025-05-07 10:08:34 +0200

    UOLOS2-376: Adjust logic to add address from course or provider according to view_on_map value in the backend

--- components/blocks/application-form/TransportArrangement.vue
+++ components/blocks/application-form/TransportArrangement.vue
@@ -179,15 +179,15 @@ const fetchCourseContent = async (nid: any) => {
   } catch (error) {
     console.error('Error fetching courses:', error);
   }
-
+console.log('courseContent.value.content', courseContent.value.content);
   // Set address, postal code and city from course content
-  if (courseContent.value.content.field_dawa_address) {
+  if (courseContent.value.content.field_dawa_address && courseContent.value.content.field_view_on_map === 'show_alternative_address') {
     handleCourseAddressChange(courseContent.value.content.field_dawa_address);
     courseAddress.value = courseContent.value.content.field_dawa_address.value;
-  } else if (courseContent.value.content.provider?.field_dawa_address) {
+  } else if (courseContent.value.content.provider?.field_dawa_address && courseContent.value.content.field_view_on_map === 'show_vendor_address') {
     handleCourseAddressChange(courseContent.value.content.provider?.field_dawa_address);
     courseAddress.value = courseContent.value.content.provider?.field_dawa_address.value;
-  } else if (courseContent.value.content.corporation?.field_dawa_address) {
+  } else if (courseContent.value.content.corporation?.field_dawa_address && courseContent.value.content.field_view_on_map === 'show_vendor_address') {
     handleCourseAddressChange(courseContent.value.content.corporation?.field_dawa_address);
     courseAddress.value = courseContent.value.content.corporation?.field_dawa_address.value;
   }
